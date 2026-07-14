0ecd1c38875f1880a7069d6d0ce7e1472e4e1c7c
emyrael <emyraeleson@gmail.com>
2026-07-14 02:18:32 +0200

    added ginja assistance flow to web

--- frontend/lib/appConstants.js
+++ frontend/lib/appConstants.js
@@ -4,3 +4,4 @@ export const APP_PLATFORMS = 'iOS & Android';
 
 export const APP_STORE_URL = 'https://apps.apple.com/us/app/ginja-app/id6752789324';
 export const GOOGLE_PLAY_URL = 'https://play.google.com/store/apps/details?id=com.ginjaapp.ginja&pcampaignid=web_share';
+export const EXPLORE_DEEP_LINK_URL = 'https://app.ginja.io/explore';
